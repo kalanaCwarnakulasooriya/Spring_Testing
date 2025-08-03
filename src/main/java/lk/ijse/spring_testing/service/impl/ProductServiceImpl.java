f03a4cdd997385039049fbd96c56914a3a9d2c98
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-03 23:51:31 +0530

    Product Service Impl

---
+++ src/main/java/lk/ijse/spring_testing/service/impl/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package lk.ijse.spring_testing.service.impl;
+
+import lk.ijse.spring_testing.entity.Product;
+import lk.ijse.spring_testing.repo.ProductRepository;
+import lk.ijse.spring_testing.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(
+                ()->new IllegalArgumentException
+                        ("Product not found"));
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Product product) {
+        Product excpProduct = getProductById(product.getId());
+        excpProduct.setName(product.getName());
+        excpProduct.setPrice(product.getPrice());
+        excpProduct.setQuantity(product.getQuantity());
+        return productRepository.save(excpProduct);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        Product product=getProductById(id);
+        productRepository.delete(product);
+    }
+}
