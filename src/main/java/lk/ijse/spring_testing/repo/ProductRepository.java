8aae426263e9743ca6de78d3622f42576f990841
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-03 23:49:16 +0530

    Product Repository

---
+++ src/main/java/lk/ijse/spring_testing/repo/ProductRepository.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring_testing.repo;
+
+import lk.ijse.spring_testing.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends
+        JpaRepository<Product,Long> {
+}
